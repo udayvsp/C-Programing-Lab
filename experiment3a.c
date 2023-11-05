096e190363e0fb6dd796bb461088792e32bf4657
udayvsp <146770886+udayvsp@users.noreply.github.com>
2023-11-05 22:43:52 +0530

    Add files via upload

---
+++ experiment3a.c
@@ -0,0 +1,16 @@
+//wap to check whether the number is odd or even 
+#include<stdio.h>
+int main()
+{
+    int a=5;
+    scanf("%d",&a);
+    if(a%2==0)
+    {
+        printf("it is even");
+    }
+    else
+    {
+        printf("it is odd");
+    }
+    return 0;
+}
